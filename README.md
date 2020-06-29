7ff84b839ed1da7324d1cae1fd6090c32486b10e
James Nathan Belue <computerghost@hotmail.com>
2020-06-28 22:27:20 -0500

    Switched Spotify API to use evilarceus sp-playcount-librespot, which will hopefully be more reliable.

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ Automatically tweet notifications of milestones for a fandom, pulled from multip
 
 You will need a database and the useful files in the `setup` folder..  Create the database, then import the `database.sql` file to create the tables and views.  While you're there, go ahead and put in the milestones.  You can use the spreadsheet and regular expression magic like I do, or you can just run `milestones.sql` for some good defaults.
 
-After the database is set up, run FanChart.  Since it's the first time running, it will prompt for some connection information.  You should be able to figure out the connection string.  And for the APIs, check out Twitter, YouTube, and [evilarceus/Spotify-Playcount](https://github.com/evilarceus/Spotify-PlayCount).
+After the database is set up, run FanChart.  Since it's the first time running, it will prompt for some connection information.  You should be able to figure out the connection string.  And for the APIs, check out Twitter, YouTube, and [evilarceus/sp-playcount-librespot](https://github.com/evilarceus/sp-playcount-librespot).
 
 Once the database is created and all of the connections are configured, items to be monitored can be managed via the "List" menu options.
 
 
 ## Scheduling?
 
-Yea, it doesn't schedule automatically.  I plan to use an external tool for that, but I still need to add support for running on load before that will work.  For now, it just has to be run manually from the menu.
+You can run at any time by clicking "Sync" then "Run Now" on the menu, but that's only good for debugging and special cases.  It's much more useful to have it all run on a schedule!
+
+An external tool must be used for scheduling.  I use Windows' Task Scheduler.  Have it run FanChart with the "--run-now" flag, which will just run the sync and close without ever showing the GUI.
 
--- src/FanChart/APIs/SpotifyAPI.vb
+++ src/FanChart/APIs/SpotifyAPI.vb
@@ -4,6 +4,8 @@ Imports Newtonsoft.Json
 
 Public Class SpotifyAPI
 
+    ' Deserialized structures:
+
     Structure Track
         Property name As String
         Property playcount As String
@@ -12,24 +14,46 @@ Public Class SpotifyAPI
         Property uri As String
     End Structure
 
+    Structure Disc
+        Property number As Integer
+        Property tracks As Track()
+    End Structure
+
+    Structure Album
+        Property discs As Disc()
+    End Structure
+
     Structure AlbumPlayCountResponse
         Property success As Boolean
-        Property data As Track()
+        Property data As Album
     End Structure
+
 
     Private EndPoint As String
 
     Sub New()
-        EndPoint = My.Settings.SpotifyEndpoint
+        EndPoint = My.Settings.SpotifyEndpoint.TrimEnd("/"c)
     End Sub
 
     Async Function GetAlbumPlayCountAsync(albumId As String) As Task(Of Track())
+
+        Dim response As AlbumPlayCountResponse
         Using client As New WebClient()
             client.Encoding = Encoding.UTF8
-            Dim json = Await client.DownloadStringTaskAsync(EndPoint & "?albumid=" & albumId)
-            Dim deserialized = JsonConvert.DeserializeObject(Of AlbumPlayCountResponse)(json)
-            Return deserialized.data
+            Dim json = Await client.DownloadStringTaskAsync(EndPoint & "/albumPlayCount?albumid=" & albumId)
+            response = JsonConvert.DeserializeObject(Of AlbumPlayCountResponse)(json)
         End Using
+
+        ' Post-process, because the info we need is nested in there and in a different format
+        Dim tracks As New List(Of Track)
+        For Each disc In response.data.discs
+            For Each track In disc.tracks
+                track.disc += 1
+                tracks.Add(track)
+            Next
+        Next
+        Return tracks.ToArray()
+
     End Function
 
 End Class
